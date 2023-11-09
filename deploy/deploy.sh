74dee78727249fcb80c9eaf13978c7bc0989e302
n.n.anikin <n.n.anikin@tinkoff.ru>
2023-11-09 14:51:52 +0300

    change ci workflow

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,5 +2,5 @@ if [ ! -f Aliaser.dmg ]; then
   bash makedmg.sh
 fi
 
-shasum -a 256 Aliaser.dmg > .checksum
+shasum -a 256 Aliaser.dmg | cut -d " " -f 1 >.checksum
 sshpass -p "$ALIASER_SRV_PWD" scp Aliaser.dmg "$ALIASER_SRV_USER"@45.8.248.195:/storage/
