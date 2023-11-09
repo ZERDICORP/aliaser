5877d3f65424e900b8ea44e7106edc758d090694
n.n.anikin <n.n.anikin@tinkoff.ru>
2023-11-09 22:27:55 +0300

    swapping

--- src/main/scala/com/zerdicorp/aliaser/App.scala
+++ src/main/scala/com/zerdicorp/aliaser/App.scala
@@ -6,7 +6,8 @@ import com.zerdicorp.aliaser.UiUtils.textChangeListener
 import javafx.beans.value.ChangeListener
 import scalafx.animation.FadeTransition
 import scalafx.application.{JFXApp, Platform}
-import scalafx.geometry.{HPos, Insets, Pos}
+import scalafx.geometry.Pos.CenterRight
+import scalafx.geometry.{HPos, Insets}
 import scalafx.scene.Scene
 import scalafx.scene.control.{Button, Label, ScrollPane, TextField}
 import scalafx.scene.layout._
@@ -19,6 +20,8 @@ import scala.collection.mutable
 
 object App extends JFXApp {
   private lazy val records = mutable.LinkedHashMap[AliasId, AliasRecord]()
+  private var firstSelectedRecord: AliasId = -1 // for changing order
+  private var secondSelectedRecord: AliasId = -1 // for changing order
   private val scheduledExecutor = Executors.newScheduledThreadPool(16)
   private var autosaveJob: Option[ScheduledFuture[_]] = None
   private var statusChangingInProgress: Boolean = false
@@ -28,6 +31,12 @@ object App extends JFXApp {
   private var scrollPane: ScrollPane = new ScrollPane()
   private val defaultScrollPaneStyle: String = "-fx-background-color: transparent;" +
     "-fx-border-color: transparent transparent gray transparent;"
+  private val selectBtnDefaultStyle: String = "-fx-cursor: hand;" +
+    "-fx-background-color: #141414;" +
+    "-fx-text-fill: gray;" +
+    "-fx-border-color: gray;" +
+    "-fx-background-radius: 5;" +
+    "-fx-border-radius: 5;"
 
   private val recordsBox = new VBox {
     spacing = 5
@@ -46,6 +55,21 @@ object App extends JFXApp {
   statusDisplayTransition.fromValue = 0.0
   statusDisplayTransition.toValue = 1.0
 
+  private val swapButton = new Button {
+    val defaultStyle: String = "-fx-cursor: hand;" +
+      "-fx-background-color: white;" +
+      "-fx-text-fill: #141414;" +
+      "-fx-font-size: 10pt;" +
+      "-fx-background-radius: 5;" +
+      "-fx-pref-width: 80;" +
+      "-fx-pref-height: 30;" +
+      "-fx-font-weight: bold"
+    text = "[ swap ]"
+    style = defaultStyle
+    onAction = _ => swapRecords()
+    disable = true
+  }
+
   private val addButton = new Button {
     text = "+ Добавить"
     style = "-fx-cursor: hand;" +
@@ -58,6 +82,60 @@ object App extends JFXApp {
     onAction = _ => addRecord()
   }
 
+  def swapRecords(): Unit = {
+    // swap in linked list
+    val temp = records(firstSelectedRecord)
+    records(firstSelectedRecord) = records(secondSelectedRecord)
+    records(secondSelectedRecord) = temp
+    // swap in records box
+    autosave(
+      delayMs = 0,
+      _ => {
+        Platform.runLater(() => {
+          records.clear()
+          recordsBox.children.clear()
+          readDotFile().reverse.map(AliasRecord.fromString(_).some).foreach(addRecord)
+          swapButton.disable = true
+          firstSelectedRecord = -1
+          secondSelectedRecord = -1
+        })
+      },
+    )
+  }
+
+  def delayedJob(duration: Int, _type: TimeUnit)(f: => Unit): ScheduledFuture[_] =
+    scheduledExecutor.schedule(
+      new Runnable {
+        def run(): Unit = f
+      },
+      duration,
+      _type,
+    )
+
+  def autosave(delayMs: Int, f: Unit => Unit = identity): Unit = {
+    autosaveJob.foreach(_.cancel(true))
+    autosaveJob = delayedJob(delayMs, TimeUnit.MILLISECONDS) {
+      rewriteDotFile(records.values.mkString("\n"))
+      f()
+      Platform.runLater(() => {
+        statusDisplayTransition.play()
+        gridPane.style = defaultGridPaneStyle + "-fx-border-color: green;"
+        scrollPane.style = defaultScrollPaneStyle + "-fx-border-color: transparent transparent green transparent;"
+      })
+      if (!statusChangingInProgress) {
+        statusChangingInProgress = true
+        delayedJob(1500, TimeUnit.MILLISECONDS) {
+          Platform.runLater(() => {
+            statusDisappearanceTransition.play()
+            gridPane.style = defaultGridPaneStyle
+            scrollPane.style = defaultScrollPaneStyle
+          })
+          statusChangingInProgress = false
+        }.some
+      }
+    }.some
+  }
+
   def addRecord(recordOpt: Option[AliasRecord] = None): Unit = {
     val record = recordOpt.getOrElse(AliasRecord.empty)
     val recordId = records.size
@@ -67,12 +145,17 @@ object App extends JFXApp {
     val recordBox = new HBox {
 //      style = "-fx-border-color: purple;"
     }
+    val selectBtn = new Button {
+      text = "::"
+      style = selectBtnDefaultStyle
+    }
     val keyField = new TextField {
       val defaultStyle: String = "-fx-border-radius: 5 0 0 5;" +
         "-fx-text-fill: white;" +
         "-fx-background-color: #141414;" +
         "-fx-border-color: gray;"
       style = defaultStyle
+      this.setAlignment(CenterRight)
     }
     val valueField = new TextField {
       val defaultStyle: String = "-fx-border-radius: 0 5 5 0;" +
@@ -94,38 +177,8 @@ object App extends JFXApp {
     keyField.setText(record.key)
     valueField.setText(record.value)
 
-    HBox.setHgrow(keyField, Priority.Always)
     HBox.setHgrow(valueField, Priority.Always)
-
-    def delayedJob(duration: Int, _type: TimeUnit)(f: => Unit): ScheduledFuture[_] =
-      scheduledExecutor.schedule(
-        new Runnable { def run(): Unit = f },
-        duration,
-        _type,
-      )
-
-    def autosave(delayMs: Int): Unit = {
-      autosaveJob.foreach(_.cancel(true))
-      autosaveJob = delayedJob(delayMs, TimeUnit.MILLISECONDS) {
-        rewriteDotFile(records.values.mkString("\n"))
-        Platform.runLater(() => {
-          statusDisplayTransition.play()
-          gridPane.style = defaultGridPaneStyle + "-fx-border-color: green;"
-          scrollPane.style = defaultScrollPaneStyle + "-fx-border-color: transparent transparent green transparent;"
-        })
-        if (!statusChangingInProgress) {
-          statusChangingInProgress = true
-          delayedJob(1500, TimeUnit.MILLISECONDS) {
-            Platform.runLater(() => {
-              statusDisappearanceTransition.play()
-              gridPane.style = defaultGridPaneStyle
-              scrollPane.style = defaultScrollPaneStyle
-            })
-            statusChangingInProgress = false
-          }.some
-        }
-      }.some
-    }
+    HBox.setHgrow(recordBox, Priority.Always)
 
     val changeFlow: (AliasRecord => String => AliasRecord) => FieldType => ChangeListener[String] = { f => fieldType =>
       textChangeListener { text =>
@@ -170,8 +223,38 @@ object App extends JFXApp {
     valueField.textProperty.addListener(changeFlow(r => t => r.copy(value = t))(FieldType.ValueField))
 
     recordBox.children = List(keyField, valueField)
-    box.children = List(recordBox, removeBtn)
+    box.children = List(selectBtn, recordBox, removeBtn)
     recordsBox.children.insert(0, box)
+
+    selectBtn.onAction = _ => {
+      if (firstSelectedRecord == recordId || secondSelectedRecord == recordId) {
+        if (firstSelectedRecord == recordId) {
+          firstSelectedRecord = -1
+          selectBtn.style = selectBtnDefaultStyle
+        } else if (secondSelectedRecord == recordId) {
+          secondSelectedRecord = -1
+          selectBtn.style = selectBtnDefaultStyle
+        }
+        swapButton.disable = true
+      } else {
+        if (firstSelectedRecord == -1) {
+          firstSelectedRecord = recordId
+          selectBtn.style = selectBtnDefaultStyle + "-fx-text-fill: cyan;" +
+            "-fx-border-color: cyan;" +
+            "-fx-font-weight: bold"
+        } else {
+          if (secondSelectedRecord == -1) {
+            secondSelectedRecord = recordId
+            selectBtn.style = selectBtnDefaultStyle + "-fx-text-fill: yellow;" +
+              "-fx-border-color: yellow;" +
+              "-fx-font-weight: bold"
+          }
+        }
+        if (firstSelectedRecord != -1 && secondSelectedRecord != -1) {
+          swapButton.disable = false
+        }
+      }
+    }
 
     removeBtn.onAction = _ => {
       records.remove(recordId)
@@ -189,25 +272,24 @@ object App extends JFXApp {
     records.addAll(copy)
   }
 
-  stage = new JFXApp.PrimaryStage { self =>
+  stage = new JFXApp.PrimaryStage { selfStage =>
     title = "Aliaser"
     resizable = false
-    scene = new Scene(400, 500) {
+    scene = new Scene(500, 600) {
       private val borderPane = new BorderPane {
         private val vbox = new VBox() {
           spacing = 10
           padding = Insets(10)
-          alignment = Pos.Center
           gridPane = new GridPane {
             vgap = 10
             scrollPane = new ScrollPane {
               padding = Insets(5)
               content = recordsBox
+              style = defaultScrollPaneStyle
+              this.setFitToHeight(true)
+              this.setFitToWidth(true)
             }
-            scrollPane.setFitToHeight(true)
-            scrollPane.setFitToWidth(true)
-            scrollPane.style = defaultScrollPaneStyle
-            self.setOnShown(_ => scrollPane.lookup(".viewport").setStyle("-fx-background-color: #141414;"))
+            selfStage.setOnShown(_ => scrollPane.lookup(".viewport").setStyle("-fx-background-color: #141414;"))
 
             val colConst = new ColumnConstraints
             colConst.percentWidth = 100
@@ -224,7 +306,7 @@ object App extends JFXApp {
             style = defaultGridPaneStyle
           }
           VBox.setVgrow(gridPane, Priority.Always)
-          children = List(gridPane)
+          children = List(swapButton, gridPane)
           style = "-fx-background-color: #141414;"
         }
         center = vbox
