f8ea5a68574823bf6cff895f414d9865ea0d3b40
n.n.anikin <n.n.anikin@tinkoff.ru>
2023-11-15 10:41:09 +0300

    makeup

--- src/main/scala/com/zerdicorp/aliaser/UiUtils.scala
+++ src/main/scala/com/zerdicorp/aliaser/UiUtils.scala
@@ -5,7 +5,6 @@ import javafx.beans.value.{ChangeListener, ObservableValue}
 object UiUtils {
   def textChangeListener(f: String => Unit): ChangeListener[String] =
     new ChangeListener[String]() {
-      override def changed(observableValue: ObservableValue[_ <: String], t: String, text: String): Unit =
-        f(text)
+      override def changed(observableValue: ObservableValue[_ <: String], t: String, text: String): Unit = f(text)
     }
 }
